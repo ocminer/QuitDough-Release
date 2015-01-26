116c88691a077b8559ca7a4144d85cafcb722a26
oc miner <you@example.com>
2015-01-26 22:47:52 +0100

    new genesis

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define QuitDough-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "QuitDoughURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
